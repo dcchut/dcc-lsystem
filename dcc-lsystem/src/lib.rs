b09d01e48884a3d11b7e11cbc87ca271edec6943
Robert Usher <266585+dcchut@users.noreply.github.com>
2020-02-10 20:42:30 +1100

    Remove needless fn main() in doctest

--- dcc-lsystem/src/lib.rs
+++ dcc-lsystem/src/lib.rs
@@ -123,33 +123,31 @@ use image::Rgb;
 use dcc_lsystem::renderer::{Renderer, VideoRendererOptionsBuilder};
 use dcc_lsystem::turtle::{TurtleAction, TurtleLSystemBuilder};
 
-fn main() {
-    let mut builder = TurtleLSystemBuilder::new();
+let mut builder = TurtleLSystemBuilder::new();
 
-    builder
-        .token("F", TurtleAction::Forward(30))
-        .token("+", TurtleAction::Rotate(90))
-        .token("-", TurtleAction::Rotate(-90))
-        .axiom("F")
-        .rule("F => F + F - F - F + F");
+builder
+    .token("F", TurtleAction::Forward(30))
+    .token("+", TurtleAction::Rotate(90))
+    .token("-", TurtleAction::Rotate(-90))
+    .axiom("F")
+    .rule("F => F + F - F - F + F");
 
-    let (mut system, renderer) = builder.finish();
-    system.step_by(5);
+let (mut system, renderer) = builder.finish();
+system.step_by(5);
 
-    let options = VideoRendererOptionsBuilder::new()
-        .filename("koch_curve.gif")
-        .fps(20)
-        .skip_by(0)
-        .padding(10)
-        .thickness(4.0)
-        .fill_color(Rgb([255u8, 255u8, 255u8]))
-        .line_color(Rgb([0u8, 0u8, 100u8]))
-        .progress_bar(true)
-        .build();
+let options = VideoRendererOptionsBuilder::new()
+    .filename("koch_curve.gif")
+    .fps(20)
+    .skip_by(0)
+    .padding(10)
+    .thickness(4.0)
+    .fill_color(Rgb([255u8, 255u8, 255u8]))
+    .line_color(Rgb([0u8, 0u8, 100u8]))
+    .progress_bar(true)
+    .build();
 
-    renderer
-        .render(&system, &options);
-}
+renderer
+    .render(&system, &options);
 ```
 
 ### Turtle actions
