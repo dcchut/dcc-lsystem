14033cbde296f3fc6e9e6914f5beaebafef5b585
eadf <ead.fritz@gmail.com>
2021-05-16 12:51:19 +0200

    Moved all of the (optional) image renderer code into a separate module.

--- dcc-lsystem/src/lib.rs
+++ dcc-lsystem/src/lib.rs
@@ -255,6 +255,8 @@ pub mod arena;
 pub mod builder;
 #[cfg(feature = "image_renderer")]
 pub mod image;
+#[cfg(feature = "image_renderer")]
+pub mod image_renderer;
 pub mod renderer;
 pub mod system;
 pub mod token;
